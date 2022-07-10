7a18e382790b3b48e07084ef43bd44387abce877
Hana Harencarova <h.harencarova@gmail.com>
2022-07-10 09:01:56 +0200

    Add user

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  validates :first_name, :last_name, :email, :encrypted_password, presence: true
+  validates :email, uniqueness: true, format: { with: /@/, message: I18n.t('user.valid_email')}
+
+  def admin?
+    is_admin == true
+  end
+
+  def name
+    [first_name, last_name].compact.join(' ')
+  end
+end
